61966604ae964bf35c1dbdeaba7b2b6fd6f27e4d
maycon huanca <mayconfelipe164@gmail.com>
2025-03-24 20:53:37 -0300

    add sketch form add register

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -3,15 +3,15 @@
 import { Header } from '@/components/Header';
 import { usePathname } from 'next/navigation';
 
-const MainLayout = ({ children }: {children : React.ReactNode}) => {
-    const pathname = usePathname();
-    console.log(pathname)
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const urls = ["/login"]
+
   return (
     <div>
-      
       <main>{children}</main>
-      {pathname !== '/login' && <Header />}
-
+      {!urls.includes(pathname) && <Header />}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import LoginPage from "./login/page";
 
 export default function Home() {
-    return <LoginPage/>
+
+    return <div>HOME</div>
 }
