6778a803a3b935e66ffba95fe864e8a995f37012
maycon.huanca <mayconfelipe164@gmail.com>
2025-04-30 18:56:28 -0300

    add interceptor to send token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,16 @@
-import Axios from "axios";
+import Axios, { InternalAxiosRequestConfig } from "axios";
 
 export const api = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = "application/json";
+    const token = localStorage.getItem("token");
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+  }
+  return config;
+}
+
+api.interceptors.request.use(authRequestInterceptor);
