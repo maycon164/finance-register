0dbeafde4e347b31cd8eb66f994dbc85645f0f30
maycon <mayconfelipe164@email.com>
2025-04-30 23:49:57 -0300

    refact css

--- src/app/calendar/components/RegisterCard/index.tsx
+++ src/app/calendar/components/RegisterCard/index.tsx
@@ -1,35 +1,40 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { RegisterType } from "@/models";
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { formatToBRL } from "@/utils";
 
 interface RegisterCardProps {
   type: RegisterType;
   value: number;
-  username: string;
+  email?: string;
 }
 
 export function RegisterCard(props: Readonly<RegisterCardProps>) {
-  const { type, value, username } = props;
+  const { type, value, email } = props;
 
   const isIncome = type === RegisterType.INCOME;
 
   return (
-    <Card className="w-full max-w-sm shadow-md border border-gray-200">
-      <CardContent className="flex flex-col ">
+    <Card
+      className={`w-full max-w-sm shadow-md rounded-none border-l-4 border-l-${
+        isIncome ? "green" : "red"
+      }-600`}
+    >
+      <CardContent className="flex flex-col">
         <div className="flex items-center justify-between">
           <h3 className="text-lg font-semibold">
-            {isIncome ? "Entrada" : "Saída"}
+            {isIncome ? "ENTRADA" : "SAIDA"}
           </h3>
-          {isIncome ? (
-            <ArrowUp className="text-green-500" />
-          ) : (
-            <ArrowDown className="text-red-500" />
-          )}
+
+          <p
+            className={`text-xl font-bold text-${
+              isIncome ? "green" : "red"
+            }-600`}
+          >
+            {formatToBRL(isIncome ? value : value * -1)}
+          </p>
         </div>
-        <p className="text-xl font-bold ${isIncome ? 'text-green-600' : 'text-red-600'}">
-          ${value.toFixed(2)}
-        </p>
-        <p className="text-sm text-gray-500">Registered by: {username}</p>
+
+        <p className="text-sm text-gray-500">Registrado por: {email}</p>
       </CardContent>
     </Card>
   );
