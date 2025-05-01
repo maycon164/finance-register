d9b77605c02972efeafc45b41f540730bb97c6db
maycon <mayconfelipe164@email.com>
2025-04-30 23:49:45 -0300

    add total value

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -3,13 +3,23 @@
 import { useState } from "react";
 import { RegisterCard } from "./components/RegisterCard";
 import WrapperCalendarWithForm from "./components/WrapperCalendar";
-import { formatDate } from "@/utils";
+import { formatDate, formatToBRL } from "@/utils";
 import { useGetRegisters } from "./api/get-registers";
+import { RegisterType } from "@/models";
 
 export default function CalendarPage() {
   const [date, setDate] = useState<Date>(new Date());
 
-  const registers = useGetRegisters();
+  const registers = useGetRegisters({ date });
+
+  const totalValue =
+    registers.data?.reduce((acc, register) => {
+      if (register.type === RegisterType.OUTCOME) {
+        return acc - register.value;
+      }
+
+      return acc + register.value;
+    }, 0) ?? 0;
 
   return (
     <section>
@@ -25,14 +35,21 @@ export default function CalendarPage() {
 
           <p className="text-2xl font-semibold mb-5">{formatDate(date)}</p>
         </div>
-
-        <p className="text-3xl font-bold text-green-700">+R$500</p>
+        {totalValue != 0 && (
+          <p
+            className={`text-3xl font-bold text-${
+              totalValue > 0 ? "green" : "red"
+            }-700`}
+          >
+            {formatToBRL(totalValue)}
+          </p>
+        )}
 
         <ul>
           {registers.data?.map((register) => (
             <li className="my-5" key={register.id}>
               <RegisterCard
-                username={register.createdBy.name}
+                email={register.createdBy.email}
                 value={register.value}
                 type={register.type}
               />
